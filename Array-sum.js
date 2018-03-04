6d6af0e1d5e3ee3a14b78cd0fb3965733a8dcc79
summer00072 <35553952+summer00072@users.noreply.github.com>
2018-03-04 13:56:27 +0800

    Update Array-sum.js

--- Array-sum.js
+++ Array-sum.js
@@ -1,11 +1,9 @@
 计算给定数组arr中所有元素的总和，数组的元素均为Number类型
 
-函数式编程reduce：
-运行时间：271ms
-占用内存：60660k(感觉不准)
+eval：
+运行时间：170ms
+占用内存：14416k(感觉不准)
 
 function sum(arr) {
-   return arr.reduce(function(prev, curr, idx, arr){
-        return prev +curr;
-   });
+  return eval(arr.join("+"));
 }
