87645f3a6e468ef2221827a5a73c28b19a83aa99
summer00072 <35553952+summer00072@users.noreply.github.com>
2018-03-04 13:28:02 +0800

    Update Array-sum.js

--- Array-sum.js
+++ Array-sum.js
@@ -1,13 +1,14 @@
 计算给定数组arr中所有元素的总和，数组的元素均为Number类型
 
-常规做法：
-运行时间：171ms
-占用内存：14416k
+forEach：
+运行时间：182ms
+占用内存：14276k(感觉不准)
 
 function sum(arr) {
     var s = 0;
-    for(var i = 0; i< arr.length; i++){
-        s += arr[i];
-    }
+    arr.forEach(function(val, idx, arr){
+        s += val;
+    });
     return s;
+    
 }
